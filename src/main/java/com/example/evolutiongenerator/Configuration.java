67d9ec12e903269f049854d7a16d7bbfd4358808
Bartosz <bartek.pawlowski00@gmail.com>
2022-12-20 02:08:04 +0100

    pull test 4

--- src/main/java/com/example/evolutiongenerator/Configuration.java
+++ src/main/java/com/example/evolutiongenerator/Configuration.java
@@ -25,7 +25,7 @@ public class Configuration {
     }
 
     public int getEnergyOfOneMove() {
-        return energyOfOneMove;
+        return mapHeight;
     }
 
     public int getInitialPlantsNumber() {
