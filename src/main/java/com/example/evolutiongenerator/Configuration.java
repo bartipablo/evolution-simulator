2fe5801920d3cc38aebfa097d3d00528b880d00a
Bartosz <bartek.pawlowski00@gmail.com>
2022-12-20 02:11:48 +0100

    added initalAnimalsNumber

--- src/main/java/com/example/evolutiongenerator/Configuration.java
+++ src/main/java/com/example/evolutiongenerator/Configuration.java
@@ -7,13 +7,14 @@ public class Configuration {
     private final int mapWidth;
     private final int energyOfOneMove;
     private final int initialPlantsNumber;
+    private final int initialAnimalsNumber;
 
-
-    Configuration(int mapHeight, int mapWidth, int energyOfOneMove, int initialPlantsNumber) {
+    Configuration(int mapHeight, int mapWidth, int energyOfOneMove, int initialPlantsNumber, int initialAnimalsNumber) {
         this.mapHeight = mapHeight;
         this.mapWidth = mapWidth;
         this.energyOfOneMove = energyOfOneMove;
         this.initialPlantsNumber = initialPlantsNumber;
+        this.initialAnimalsNumber = initialAnimalsNumber;
     }
 
     public int getMapHeight() {
@@ -30,5 +31,9 @@ public class Configuration {
 
     public int getInitialPlantsNumber() {
         return initialPlantsNumber;
+    }
+
+    public int getInitialAnimalsNumber() {
+        return initialAnimalsNumber;
     }
 }
