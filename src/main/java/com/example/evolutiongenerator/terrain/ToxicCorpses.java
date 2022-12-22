a2033c80c476ab35cbac47abf84ddeb25f66df0e
Bartosz <bartek.pawlowski00@gmail.com>
2022-12-22 19:17:48 +0100

    added new simulation GUI

--- src/main/java/com/example/evolutiongenerator/terrain/ToxicCorpses.java
+++ src/main/java/com/example/evolutiongenerator/terrain/ToxicCorpses.java
@@ -47,6 +47,4 @@ public class ToxicCorpses extends AbstractTerrain {
             }
         }
     }
-
-
 }
--- src/main/java/com/example/evolutiongenerator/variants/TerrainVariant.java
+++ src/main/java/com/example/evolutiongenerator/variants/TerrainVariant.java
@@ -1,5 +1,6 @@
 package com.example.evolutiongenerator.variants;
 
 public enum TerrainVariant {
-
+    FOREST_EQUATORS,
+    TOXIC_CORPSES
 }
