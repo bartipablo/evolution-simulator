1c22b76bab39ba2a14fc9a4c95370836a7800b45
Piotr Olszak <wallshoccfella@gmail.com>
2022-12-28 11:57:09 +0100

    test added, concatTwoIntArrays changed

--- src/main/java/com/example/evolutiongenerator/Static.java
+++ src/main/java/com/example/evolutiongenerator/Static.java
@@ -53,7 +53,7 @@ public class Static {
 
     public static int[] concatTwoIntArrays(int[] array1, int[] array2) {
         int[] result = new int[array1.length + array2.length];
-        System.arraycopy(array1, 0, result, 0, array1.length - 1);
+        System.arraycopy(array1, 0, result, 0, array1.length);
         System.arraycopy(array2, 0, result, array1.length, array2.length);
         return result;
     }
--- src/test/java/StaticTest.java
+++ src/test/java/StaticTest.java
@@ -17,8 +17,6 @@ public class StaticTest {
                 new Vector2D(2, 3),
                 new Vector2D(2, 4)
         };
-        System.out.println(result);
-        System.out.println(expected);
         assertArrayEquals(expected, result);
 
         // Test with xMin > xMax
@@ -51,6 +49,47 @@ public class StaticTest {
         int[] expected2 = { 3 };
         assertArrayEquals(expected2, result);
     }
+    @Test
+    public void ConcatTwoIntArraysTest() {
+        //two non empty arrays
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {4, 5, 6};
+        int[] expected = {1, 2, 3, 4, 5, 6};
+        int[] result = concatTwoIntArrays(array1, array2);
+        assertArrayEquals(expected, result);
+
+        //two empty arrays
+        int[] array3 = {};
+        int[] array4 = {};
+        int[] expected1 = {};
+        int[] result1 = concatTwoIntArrays(array3, array4);
+        assertArrayEquals(expected1, result1);
+
+        //test ConcatEmptyArray And Non Empty Array
+
+        int[] array5 = {};
+        int[] array6 = {1, 2, 3};
+        int[] expected2 = {1, 2, 3};
+        int[] result2 = concatTwoIntArrays(array5, array6);
+        assertArrayEquals(expected2, result2);
+
+        int[] array7 = {1, 2, 3};
+        int[] array8 = {};
+        int[] expected3 = {1, 2, 3};
+        int[] result3 = concatTwoIntArrays(array3, array4);
+        assertArrayEquals(expected2, result2);
+
+
+        //test ConcatTwoArrays With Single Element Each
+
+        int[] array9 = {1};
+        int[] array0 = {2};
+        int[] expected4 = {1, 2};
+        int[] result4 = concatTwoIntArrays(array9, array0);
+        assertArrayEquals(expected4, result4);
+
+    }
+
 }
 
 
